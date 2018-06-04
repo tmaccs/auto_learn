d561e122213f34f2b5f30c5f11ee4fd406120b73
Tmaccs <tmaccs@live.com>
2018-06-04 22:06:01 +0800

    auto study for a website

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+START_URL = 'http://www.cdjxjy.com/SiteWeb/'
+USER_NAME = 'sazx201408'
+PASSWORD = '11111111'
+SUBJECT = u'化学'
+STUDY_URL = 'http://www.cdjxjy.com/student/SelectCourseRecord.aspx'
+
---
+++ course_selection.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class CourseSelection:
+    def __init__(self):
+        pass
+

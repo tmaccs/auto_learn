793844cadb76ad7f3f372f62ab4f3564186e8ca4
Ian <tmaccs@live.com>
2018-06-09 15:01:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 START_URL = 'http://www.cdjxjy.com/SiteWeb/'
-USER_NAME = 'sazx201408'
-PASSWORD = '11111111'
+USER_NAME = 'aaaaaaaaa'
+PASSWORD = '********'
 SUBJECT = u'化学'
 STUDY_URL = 'http://www.cdjxjy.com/student/SelectCourseRecord.aspx'
 
